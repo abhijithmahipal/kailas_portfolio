20670cd574f7b9e9f570a939b948caa6f30fa98e
abhijithmahipal <abhijithmahipal@gmail.com>
2023-06-04 21:09:54 +0530

    fixed scroll reset on refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'kailas';
+
+  @HostListener('window:beforeunload', ['$event'])
+  onBeforeUnload(event: Event): void {
+    window.scrollTo(0, 0);
+  }
 }
