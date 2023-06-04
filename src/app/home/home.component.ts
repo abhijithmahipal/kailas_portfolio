fde1817c26d128a47dc09140d59f5fc0c39cba62
abhijithmahipal <abhijithmahipal@gmail.com>
2023-06-04 15:28:01 +0530

    ui enhancements in homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const options = {
+      strings: ['Hello,'],
+      typeSpeed: 200,
+      showCursor: false,
+    };
 
+    const typed = new Typed('.hello-text', options);
+  }
 }
